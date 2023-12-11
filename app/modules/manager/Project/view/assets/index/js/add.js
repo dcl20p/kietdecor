78f0cff2c49a323de83efb92cc99f649a67dd8c1
Tungdev <nesv045@gmail.com>
2023-12-11 10:48:59 +0700

    tesst

--- app/modules/manager/Project/view/assets/index/js/add.js
+++ app/modules/manager/Project/view/assets/index/js/add.js
@@ -135,7 +135,7 @@
 
     const quillDes  = elDes && common.initQuill(elDes);
     const metaDes   = elMetaDes && common.initQuill(elMetaDes);
-    const dropzoneListImg   = elImage && common.initDropzone(elImage, {maxFiles: 50}, listThumbnails);
+    const dropzoneListImg   = elImage && common.initDropzone(elImage, {maxFiles: 50}, listThumbnails); 
     const dropzoneThumbnail = elImage && common.initDropzone(elThumbnail, {maxFiles: 1}, thumbnails); 
     
     elMetaKeyword && common.initChoicesTags(elMetaKeyword);
